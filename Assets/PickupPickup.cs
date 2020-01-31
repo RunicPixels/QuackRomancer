5fb172b7cf1705066114bca933f7b30cc3c32fb8
Cheezegami <superronald@live.nl>
2020-02-01 00:18:15 +0100

    Trying to visualize pickup in UI based camera (not finished yet)

---
+++ Assets/PickupPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupPickup : MonoBehaviour
+{
+    public PickUpManager pickUpManager;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PickUp>() != null)
+        {
+            var pickup = other.GetComponent<PickUp>();
+            pickUpManager.AddPickUp(pickup);
+            pickup.transform.parent = pickUpManager.transform;
+        }
+    }
+}
--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickUp : MonoBehaviour
 {
     enum PickupType { Hat, Head, Body }
+
+    public void OnPickup()
+    {
+        transform.position += Vector3.down * 5f;
+        gameObject.layer = 5;
+        gameObject.AddComponent<LayoutElement>();
+    }
 }
