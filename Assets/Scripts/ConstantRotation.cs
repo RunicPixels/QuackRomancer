f736f8a632bd68e278dbf81a264ab3d3b80fc205
Cheezegami <superronald@live.nl>
2020-01-31 21:35:48 +0100

    Rotating Hat Pickup

---
+++ Assets/Scripts/ConstantRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotation : MonoBehaviour
+{
+    public Vector3 axis = Vector3.up;
+
+    public float amount = 50f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Rotate(axis, amount * Time.deltaTime);
+    }
+}
