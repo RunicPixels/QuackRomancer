9ecaad3cacadd9504dfe88838342005e16b5b343
Phisamoe <sofietje.vos@gmail.com>
2020-01-31 23:16:43 +0100

    pickup inventory now has some function

---
+++ Assets/Scripts/PickUpManagerEntry.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpManagerEntry : MonoBehaviour
+{
+    public Text textObject;
+
+    public void SetPickUp(PickUp pickUp) {
+        textObject.text = pickUp.gameObject.name;
+    }
+}
