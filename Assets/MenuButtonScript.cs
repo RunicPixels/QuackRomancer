5c3a526c026fe4122ff4230224b8f32f9d4818ff
Frank <f-rombouts@hotmail.com>
2020-02-02 01:02:17 +0100

    Fixes

--- Assets/MenuButtonScript.cs
+++ Assets/MenuButtonScript.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(AudioSource))]
+
+
 public class MenuButtonScript : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public void PlayGame()
     {
         SceneManager.LoadScene("RonaldTestScene");
+=======
+    public AudioClip click;
+    public AudioClip clickQuit;
+    AudioSource audioSourceClick;
+
+
+    private void Awake()
+    {
+        audioSourceClick = GetComponent<AudioSource>();
+    }
+
+public void PlayGame()
+    {
+
+        audioSourceClick.PlayOneShot(click, 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+>>>>>>> Stashed changes
     }
 
     public void ReplayGame()
@@ -17,6 +39,7 @@ public class MenuButtonScript : MonoBehaviour
 
     public void QuitGame ()
     {
+        audioSourceClick.PlayOneShot(clickQuit, 1);
         Debug.Log("Quit!");
         Application.Quit();
     }
--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(AudioSource))]
 public class PickUp : MonoBehaviour
 {
     public enum PickupType { Beak, Head, Body, Tail }
@@ -11,10 +12,19 @@ public class PickUp : MonoBehaviour
     public string pickupName;
     public bool hasBeenPickedUp = false;
 
+    public AudioClip pickup;
+    AudioSource audioSourcePickup;
+
+    private void Awake()
+    {
+        audioSourcePickup = GetComponent<AudioSource>();
+    }
+
     public void OnPickup()
     {
         transform.position += Vector3.down * 5f;
         gameObject.layer = 5;
         gameObject.AddComponent<LayoutElement>();
+        audioSourcePickup.PlayOneShot(pickup, 1);
     }
 }
