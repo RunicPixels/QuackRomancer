d46abf2b7f67a28eb48b802d906eae4e8dc16827
Phisamoe <sofietje.vos@gmail.com>
2020-02-02 12:52:52 +0100

    duck reacts properly now

--- Assets/Scripts/ResultsManager.cs
+++ Assets/Scripts/ResultsManager.cs
@@ -8,7 +8,7 @@ public class ResultsManager : MonoBehaviour
     public GameObject Curtains;
     public GameObject[] winningCombos;
     public GameObject turd;
-
+    public Animator duckReactionAnimator;
 
     void Start() {
         for (int i = 0; i < winningCombos.Length; i++) {
@@ -23,11 +23,20 @@ public class ResultsManager : MonoBehaviour
             // sad stuff, show sad animation!
             turd.SetActive(true);
         }
+        duckReactionAnimator.SetBool("LovelyReaction", false);
+        duckReactionAnimator.SetBool("DisgustedReaction", false);
         StartCoroutine(ExampleCoroutine());
     }
 
     IEnumerator ExampleCoroutine() {
         yield return new WaitForSeconds(2);
+
+        if (PickUpManager.generatedComboNumber != -1) {
+            duckReactionAnimator.SetBool("LovelyReaction", true);
+        }
+        else {
+            duckReactionAnimator.SetBool("DisgustedReaction", true);
+        }
 
         for (int i = 0; i < 350; i++)
         {
@@ -36,6 +45,7 @@ public class ResultsManager : MonoBehaviour
         }
 
         Curtains.SetActive(false);
+
 
         yield return new WaitForSeconds(8);
 
