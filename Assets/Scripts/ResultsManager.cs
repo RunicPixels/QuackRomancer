373e766a8a7e86f8320e1b996f49e213639c9592
Phisamoe <sofietje.vos@gmail.com>
2020-02-02 03:47:12 +0100

    it is now possible to create actual poop in-game

--- Assets/Scripts/ResultsManager.cs
+++ Assets/Scripts/ResultsManager.cs
@@ -6,19 +6,20 @@ public class ResultsManager : MonoBehaviour
 {
     public GameObject dundundunpanel;
     public GameObject[] winningCombos;
-
+    public GameObject turd;
 
     void Start() {
         for (int i = 0; i < winningCombos.Length; i++) {
             winningCombos[i].SetActive(false);
         }
+        turd.SetActive(false);
         if (PickUpManager.generatedComboNumber != -1) {
             // happy stuff, show happy animation!
             winningCombos[PickUpManager.generatedComboNumber].SetActive(true);
         }
         else {
             // sad stuff, show sad animation!
-
+            turd.SetActive(true);
         }
         StartCoroutine(ExampleCoroutine());
     }
